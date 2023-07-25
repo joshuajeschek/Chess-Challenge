5f1356c42ebe7acf985773b2cefb9ba10d14f139
Joshua Jeschek <joshua@jeschek.eu>
2023-07-25 11:41:10 +0200

    feat: add stockfish bot

---
+++ Chess-Challenge/src/Stockfish/Stockfish.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using ChessChallenge.API;
+
+public class Stockfish : IChessBot {
+    private Process stockfishProcess;
+    private StreamWriter Ins() => stockfishProcess.StandardInput;
+    private StreamReader Outs() => stockfishProcess.StandardOutput;
+
+    public Stockfish() {
+        var stockFishBin = Environment.GetEnvironmentVariable("STOCKFISH_BIN");
+        if (stockFishBin == null) {
+            throw new Exception("Missing environment variable: 'STOCKFISH_BIN'");
+        }
+        var stockFishLevel = Environment.GetEnvironmentVariable("STOCKFISH_LEVEL");
+        if (stockFishLevel == null) {
+            throw new Exception("Missing environment variable: 'STOCKFISH_LEVEL'");
+        }
+        int skillLevel = Int32.Parse(stockFishLevel);
+        stockfishProcess = new();
+        stockfishProcess.StartInfo.RedirectStandardOutput = true;
+        stockfishProcess.StartInfo.RedirectStandardInput = true;
+        stockfishProcess.StartInfo.FileName = stockFishBin;
+        stockfishProcess.Start();
+
+        Ins().WriteLine("uci");
+        string? line;
+        var isOk = false;
+
+        while ((line = Outs().ReadLine()) != null) {
+            if (line == "uciok") {
+                isOk = true;
+                break;
+            }
+        }
+
+        if (!isOk) {
+            throw new Exception("Failed to communicate with stockfish");
+        }
+
+        Ins().WriteLine($"setoption name Skill Level value {skillLevel}");
+    }
+
+    public Move Think(Board board, Timer timer) {
+        Ins().WriteLine("ucinewgame");
+        Ins().WriteLine($"position fen {board.GetFenString()}");
+        var timeString = board.IsWhiteToMove ? "wtime" : "btime";
+        Ins().WriteLine($"go {timeString} {timer.MillisecondsRemaining}");
+
+        string? line;
+        Move? move = null;
+
+        while ((line = Outs().ReadLine()) != null) {
+            if (line.StartsWith("bestmove")) {
+                var moveStr = line.Split()[1];
+                move = new Move(moveStr, board);
+
+                break;
+            }
+        }
+
+        if (move == null) {
+            throw new Exception("Engine crashed");
+        }
+
+        return (Move)move;
+    }
+}
