47227fc057145b4804ea9e89db38f059829e824e
Joshua Jeschek <joshua@jeschek.eu>
2023-07-26 13:02:37 +0200

    feat: improved evaluation function

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,11 +1,12 @@
 ﻿using ChessChallenge.API;
 using System;
-using static ChessChallenge.Application.ConsoleHelper;
+using System.Linq;
 
 public class MyBot : IChessBot
 {
 
-    private int[] _pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+    //                             .  P   K   B   R   Q   K
+    private int[] _pieceValues = { 0, 10, 30, 30, 50, 90, 2000 };
 
     public Move Think(Board board, Timer timer)
     {
@@ -26,23 +27,22 @@ public class MyBot : IChessBot
 
     private int Evaluate(Board board)
     {
-      if (board.IsInCheckmate()) {
-        return Int32.MinValue;
-      }
-      if (board.IsInCheck()) {
-        return Int32.MinValue / 2;
-      }
-      if (board.IsRepeatedPosition()) {
-        return Int32.MinValue / 4;
-      }
-      if (board.IsDraw()) {
-        return 0;
-      }
+      if (board.IsInCheckmate()) return Int32.MinValue;
+      if (board.IsInCheck()) return Int32.MinValue / 2;
+      if (board.IsRepeatedPosition()) return Int32.MinValue / 4;
+      if (board.IsDraw()) return 0;
+
+      int myMobility = board.GetLegalMoves().Length;
+      board.TrySkipTurn(); // we already checked for IsInCheck above
+      int theirMobility = board.GetLegalMoves().Length;
+      board.UndoSkipTurn();
+
       int score = board.GetLegalMoves().Length;
-      /* int score = 0; */
+      int[] pieceScores = { 0, 0, 0, 0, 0, 0, 0 };
       foreach(PieceList list in board.GetAllPieceLists()) {
-          score += list.IsWhitePieceList==board.IsWhiteToMove ? _pieceValues[(int)list.TypeOfPieceInList] : 0;
+          pieceScores[(int)list.TypeOfPieceInList] += list.Count * (list.IsWhitePieceList==board.IsWhiteToMove ? 1 : -1);
       }
-      return score;
+      int pieceScore = pieceScores.Select((pieceScore, index) => pieceScore * _pieceValues[index]).Sum();
+      return (myMobility - theirMobility) + pieceScore;
     }
 }
