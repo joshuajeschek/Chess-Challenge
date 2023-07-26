8179d90f888a10a6a46009c33984f7fccf5e0886
laurin657 <laurin657@gmail.com>
2023-07-26 13:28:41 +0200

    Feat: Negamax Search

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,12 +1,13 @@
 ﻿using ChessChallenge.API;
 using System;
 using System.Linq;
+using System.Data;
 
 public class MyBot : IChessBot
 {
-
     //                             .  P   K   B   R   Q   K
     private int[] _pieceValues = { 0, 10, 30, 30, 50, 90, 2000 };
+    private Move bestMove = Move.NullMove;
 
     public Move Think(Board board, Timer timer)
     {
@@ -23,6 +24,43 @@ public class MyBot : IChessBot
           }
         }
         return bestMove;
+    }
+
+    int Search(Board board, int depth, int maxdepth)
+        /*  Input
+         *
+         *  board: Board, Current board
+         *  depth: int, Current search depth
+         *  maxdepth: int, maximal depth to be searches + the depth at which the boards are ultimately evaluated
+         *
+         *  Output
+         *
+         *  bestscore: int, the score of the board at depth=maxdepth with the best score obtained
+        */
+    {
+        if (depth == maxdepth)
+        /*
+         *Evaluate returns int score, scored in respect to the next moving player. The board it is evaluating is the
+         *end board of one branch and scores will be handed back to the next node (*-1, since the black moves that creates a bad setting for white will be made and
+         *a white move that creates a bad setting for black is our right move)
+        */
+            return Evaluate(board);
+
+        int bestScore = int.MinValue ;
+        Move[] moves = board.GetLegalMoves();
+        foreach (Move move in moves)
+        {
+            board.MakeMove(move);
+            int score = -Search(board, depth+1, maxdepth);
+            if (score > bestScore)
+            {
+                bestScore = score;
+                if (depth == 0)
+                    this.bestMove = move;
+            }
+            board.UndoMove(move);
+        }
+        return bestScore;
     }
 
     private int Evaluate(Board board)
@@ -46,3 +84,5 @@ public class MyBot : IChessBot
       return (myMobility - theirMobility) + pieceScore;
     }
 }
+
+
